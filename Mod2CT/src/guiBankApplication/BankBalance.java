83b09ed0dc9a764a6049d02ecad577736e6c9071
David Owen <david.owen@csuglobal.edu>
2024-03-28 20:58:28 -0500

    different approch

---
+++ Mod2CT/src/guiBankApplication/BankBalance.java
@@ -0,0 +1,5 @@
+package guiBankApplication;
+
+public class BankBalance {
+
+}
--- Week2/src/criticalThinking/BankBalance.java
+++ Week2/src/criticalThinking/BankBalance.java
@@ -13,7 +13,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.border.Border;
+
 
 public class BankBalance extends JFrame implements ActionListener {
 	
@@ -28,67 +28,70 @@ public class BankBalance extends JFrame implements ActionListener {
 	String numStore;
 	
 
-	BankBalance() {
+	public BankBalance() {
 		
 		JFrame frame = new JFrame();
 		frame.setTitle("(GUI) Bank Balance application");
 		
 		// label for the output
 		balanceLabel = new JLabel();
-		balanceLabel.setBounds(250, 400, 100, 50);
-		balanceLabel.setForeground(Color.BLACK);
-		balanceLabel.setBackground(Color.white);
-		balanceLabel.setHorizontalAlignment(JLabel.CENTER);
-		balanceLabel.setVerticalAlignment(JLabel.CENTER);
-		balanceLabel.setFont(new Font("Ariel",Font.BOLD,15));
-		balanceLabel.setOpaque(false);
-		balanceLabel.setVisible(false);
+			balanceLabel.setBounds(250, 400, 100, 50);
+			balanceLabel.setForeground(Color.BLACK);
+			balanceLabel.setBackground(Color.white);
+			balanceLabel.setHorizontalAlignment(JLabel.CENTER);
+			balanceLabel.setVerticalAlignment(JLabel.CENTER);
+			balanceLabel.setFont(new Font("Ariel",Font.BOLD,15));
+			balanceLabel.setOpaque(false);
+			balanceLabel.setVisible(false);
 		
 		// box for input from user
 		userInput = new JTextField();
-		userInput.setPreferredSize(new Dimension(100,50));
-		userInput.setBounds(200, 50, 200, 50);
-		userInput.setEditable(true);
-		userInput.setText("Type amount here.");
-		userInput.setFont(new Font("Ariel", Font.BOLD, 14));
-		userInput.addActionListener(this);
+			userInput.setPreferredSize(new Dimension(100,50));
+			userInput.setBounds(200, 50, 200, 50);
+			userInput.setEditable(true);
+			userInput.setText("");
+			userInput.setFont(new Font("Ariel", Font.BOLD, 14));
+			userInput.setHorizontalAlignment(JTextField.CENTER);
+			userInput.addActionListener(this);
 		
 		// submit button to enter input from user into the program
-		submitButton = new JButton("Submit");
-		submitButton.addActionListener(this);
-		submitButton.setBounds(250,25, 100, 25);
+		submitButton = new JButton("Enter Balance Amount");
+			submitButton.setHorizontalAlignment(JTextField.CENTER);
+			submitButton.addActionListener(this);
+			submitButton.setFocusable(false);
+			submitButton.setBounds(200, 25, 200, 25);
 		
 		
 		// panels for organization
 		JPanel submitPanel = new JPanel();
-		submitPanel.setBackground(Color.black);
-		submitPanel.setBounds(0,0,600,200);
-		submitPanel.setLayout(new GridLayout(1,1));
+			submitPanel.setBackground(Color.black);
+			submitPanel.setBounds(0,0,600,200);
+			submitPanel.setLayout(new GridLayout(1,1));
 		JPanel processPanel = new JPanel();
-		processPanel.setBackground(Color.black);
-		processPanel.setBounds(0,200,600,200);
-		processPanel.setLayout(new GridLayout(1,2));
+			processPanel.setBackground(Color.black);
+			processPanel.setBounds(0,200,600,200);
+			processPanel.setLayout(new GridLayout(1,2));
 		JPanel outputPanel = new JPanel();
-		outputPanel.setBackground(Color.black);
-		outputPanel.setBounds(0, 400, 600, 200);
-		outputPanel.setLayout(new GridLayout(1,1));
+			outputPanel.setBackground(Color.black);
+			outputPanel.setBounds(0, 400, 600, 200);
+			outputPanel.setLayout(new GridLayout(1,1));
 		
-		
+		// Deposit Button
 		depButton = new JButton();
 		depButton.setBounds(0,200, 300, 200);
 		depButton.addActionListener(this);
-		
+		// button features
 		depButton.setText("Deposit");
 		depButton.setFocusable(false);
 		depButton.setForeground(Color.blue);
 		depButton.setBackground(Color.cyan);
 		depButton.setFont(new Font("Ariel", Font.BOLD, 25));
 		depButton.setBorder(BorderFactory.createEtchedBorder());
-		
+		// Withdrawal Button
 		withButton = new JButton();
 		withButton.setBounds(300,200, 300, 200);
 		withButton.addActionListener(this);
-		
+		// button features
 		withButton.setText("Withdrawl");
 		withButton.setFocusable(false);
 		withButton.setForeground(Color.white);
@@ -99,7 +102,7 @@ public class BankBalance extends JFrame implements ActionListener {
 		
 		
 				
-		
+		// Frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(balanceLabel);
 		frame.add(submitButton);
@@ -119,35 +122,35 @@ public class BankBalance extends JFrame implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		int tempNum = 0;
-		int tempNum1 = 0;
-		int output = 0;
-		int output2 = 0;
+		Double tempNum = 0.0;
+		Double tempNum1 = 0.0;
+		Double output = 0.0;
+		// State balance
 		if(e.getSource() == submitButton) {
 			numStore = userInput.getText();
 			balanceLabel.setText(numStore);
 			balanceLabel.setOpaque(true);
 			balanceLabel.setVisible(true);
-			
+		// Deposit button	
 		}else if(e.getSource() == depButton){
 			deposit = userInput.getText();
-			tempNum = Integer.parseInt(deposit);
-			tempNum1 = Integer.parseInt(numStore);
+			tempNum = Double.parseDouble(deposit);
+			tempNum1 = Double.parseDouble(numStore);
 			output = tempNum + tempNum1;
-			balanceLabel.setText(Integer.toString(output));
+			balanceLabel.setText(Double.toString(output));
 			balanceLabel.setOpaque(true);
 			balanceLabel.setVisible(true);
-			numStore = Integer.toString(output);
-			
+			numStore = Double.toString(output);
+		// Withdrawal button	
 		}else if(e.getSource() == withButton){
 			withdrawl = userInput.getText();
-			tempNum = Integer.parseInt(withdrawl);
-			tempNum1 = Integer.parseInt(numStore);
+			tempNum = Double.parseDouble(withdrawl);
+			tempNum1 = Double.parseDouble(numStore);
 			output = tempNum1 - tempNum;
-			balanceLabel.setText(Integer.toString(output));
+			balanceLabel.setText(Double.toString(output));
 			balanceLabel.setOpaque(true);
 			balanceLabel.setVisible(true);
-			numStore = Integer.toString(output);
+			numStore = Double.toString(output);
 		}
 	}
 }
