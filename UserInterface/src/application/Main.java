b31350cc8cf1bc381c426d3421f6f7ea89eac402
David Owen <david.owen@csuglobal.edu>
2024-04-04 14:28:15 -0500

    Comments and modification updates

--- UserInterface/src/application/Main.java
+++ UserInterface/src/application/Main.java
@@ -8,8 +8,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
-//import javafx.animation.Animation;
-//import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.EventHandler;
@@ -35,7 +33,6 @@ public class Main extends Application {
 			scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("User Interface");
-			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
@@ -73,34 +70,18 @@ public class Main extends Application {
 		Timeline box1 = new Timeline();
 		Duration duration = Duration.seconds(1);
 		
-//		// this creates the timeline
-//		Timeline box1 = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
-//	            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-//	            String currentTime = sdf.format(new Date());
-//	            box.setText(currentTime);
-//	        }));
-		   
-	        
-		
-
-		
+		// Button events
 		btn1.addEventHandler(MouseEvent.MOUSE_CLICKED,new EventHandler<MouseEvent>() {
 			
 			public void handle(MouseEvent event) {
 				// loading the SimpleDateFormat and formatting it for HH:mm assigning the sdf variable
-				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 				// loading the date utility and assigning the currentTime variable
-				Date currentTime = new Date();
 				// setting up the variable to represent the current time as formatted. 
-				String formattedTime = sdf.format(currentTime);
 				// inserting that time as a text in the box TextField
-				box.setText(formattedTime);
-				
-				
-				
-//				box1.setCycleCount(Animation.INDEFINITE);
-//				box1.play();
-				
+				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");				
+				Date currentTime = new Date();				
+				String formattedTime = sdf.format(currentTime);
+				box.setText(formattedTime);				
 			}
 		});
 		
@@ -108,10 +89,9 @@ public class Main extends Application {
 			
 			public void handle(MouseEvent event) {
 				// Assigning the text String to the information in the box TextField
-				String text = box.getText();
 				// took this out as I did not start the animation any longer
-//				box1.pause();
 				// runs saveToFile method below and uses the text TextField data
+				String text = box.getText();				
 				saveToFile(text);
 				
 			}
@@ -121,19 +101,19 @@ public class Main extends Application {
 			
 			public void handle(MouseEvent event) {
 				// Generate random int for green 
-		        Random random = new Random();
-		        // Assigned 0 for both red and blue, then gave green the random number
-		        int red = 0;
-		        int green = random.nextInt(256); 
-		        int blue = 0; 
-		        /* # is literal, % Indicates that a format specifier follows
+				// Assigned 0 for both red and blue, then gave green the random number
+				/* # is literal, % Indicates that a format specifier follows
 		         * 0 Indicates that leading zeros should be including if necessary (01,08)
 		         * 2 indicates that the output should be at least 2 characters wide (XX)
 		         * 'x' Indicates that the argument should be formatted as a hexadecimal integer
 		         * "#05fa83" or "#ffffff" or "#00000f"
 		         */
-		        String color = String.format("#%02x%02x%02x", red, green, blue); 
-		        // added this random green color to root, which was sent to the Scene
+				// added this random green color to root, which was sent to the Scene
+		        Random random = new Random();		        
+		        int red = 0;
+		        int green = random.nextInt(256); 
+		        int blue = 0; 		        
+		        String color = String.format("#%02x%02x%02x", red, green, blue); 		        
 				root.setStyle("-fx-background-color: " + color);
 		        
 			}
@@ -149,6 +129,7 @@ public class Main extends Application {
 		
 		// adds all buttons to the "btns" variable
 		btns.getChildren().addAll(btn1,btn2,btn3,btn4);
+		
 		// adds the buttons and textField to the Pane
 		root.setTop(btns);
 		root.setCenter(box);
@@ -156,13 +137,13 @@ public class Main extends Application {
 		
 	}
 	// gets an input of "text"
-	public void saveToFile(String text) {
-		// creates a file called "Log.txt" and saves it to this directory
-		File file = new File("C:\\Temp\\Log.txt");	
-		// try/catch handles the IO Exception that needed to be handled
-		// applies the FileWriter utility assigning writer as variable
-        try (FileWriter writer = new FileWriter(file)) {
-        	// writes the file
+	// creates a file called "Log.txt" and saves it to this directory
+	// try/catch handles the IO Exception that needed to be handled
+	// applies the FileWriter utility assigning writer as variable
+	// writes the file
+	public void saveToFile(String text) {		
+		File file = new File("C:\\Temp\\Log.txt");		
+        try (FileWriter writer = new FileWriter(file)) {        	
             writer.write(text);
         } catch (IOException e) {
             e.printStackTrace();
