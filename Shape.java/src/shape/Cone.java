42f60b8b4a5583621b46aa01da7ea55e0203eaf2
David Owen <david.owen@csuglobal.edu>
2024-04-14 12:57:33 -0500

    didnt commit?

--- Shape.java/src/shape/Cone.java
+++ Shape.java/src/shape/Cone.java
@@ -1,33 +1,40 @@
 package shape;
 
 public class Cone extends Shape{
-
+	
+	// variables
 	double radius;
 	double height;
 	
+	// Constructor
 	public Cone(double radius, double height) {
 		this.radius = radius;
 		this.height = height;
 	}
 	
+	// getter
 	public double getRadius() {
 		return radius;
 	}
 	
+	// getter
 	public double getHeight() {
 		return height;
 	}
 	
+	// Override shape for specific Cone method
 	@Override
 	public double surface_area() {
 		return Math.PI * radius * (radius + Math.sqrt((height * height) + (radius * radius)));
 	}
 
+	// Override shape for specific Cone method
 	@Override
 	public double volume() {
 		return (Math.PI * radius * radius * height) / 3;
 	}
 	
+	// toString for output
 	@Override
 	public String toString() {
 		return "Surface Area: " + dF.format(surface_area()) + ", Volume: " + dF.format(volume());
