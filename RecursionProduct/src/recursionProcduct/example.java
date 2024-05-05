2c71bd8828707be63341d9d531a16bf3eebeba00
David Owen <d_owen_net@outlook.com>
2024-05-05 12:28:59 -0500

    Week6 CT fixed

--- RecursionProduct/src/recursionProcduct/example.java
+++ RecursionProduct/src/recursionProcduct/example.java
@@ -1,5 +1,43 @@
 package recursionProcduct;
 
-public class example {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class example {
+    
+    public static void main(String[] args) {
+    
+        Scanner scnr = new Scanner(System.in);
+        int[] numbers = new int[5];
+        
+        System.out.println("Enter 5 numbers: ");
+        for (int i = 0; i < 5; i++) {
+            numbers[i] = getInputInt(scnr);
+        }
+        scnr.close();
+        int product = multiply(numbers, 0);
+        System.out.println("The product of the numbers is: " + product);
+    }
+    
+    public static int getInputInt(Scanner scnr) {
+        while(true) {
+            try {
+                System.out.print("Next number: ");
+                int number = scnr.nextInt();
+                if (number >= 0) {
+                    return number;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter in a valid number.");
+                scnr.nextLine();
+            }
+        }
+    }
+    
+    public static int multiply(int[] numbers, int index) {
+		if(index >= numbers.length) {
+			return 1;
+		}
+		return numbers[index] * multiply(numbers, index + 1);
+	}
 }
--- SortingStudents/src/sortingStudents/SortingStudents.java
+++ SortingStudents/src/sortingStudents/SortingStudents.java
@@ -20,34 +20,37 @@ public class SortingStudents {
 	public static void main(String[] args) {
 		
 		ArrayList<Student> students = new ArrayList<>();
-		Scanner scnr = new Scanner(System.in);
+		try (Scanner scnr = new Scanner(System.in)) {
 		 
 		
-		System.out.println("Enter Name, Roll Number, and Address. ");
-		int i = 0;
-		while (i <= 10) {
-			System.out.println("Name: ");
-			String name = scnr.next();
+			System.out.println("Enter Name, Roll Number, and Address. ");
+			int i = 0;
+			while (i <= 10) {
+				System.out.println("Name: ");
+				String name = scnr.next();
 		
-			int rollno = -1;
-			while(rollno < 0) {
-				try {
-					System.out.println("Roll Number: ");
-					rollno = scnr.nextInt();
-				}catch (InputMismatchException e) {
-					System.out.println("Invalid entry. Please enter again.");
-					scnr.next();
+				int rollno = -1;
+				while(rollno < 0) {
+					try {
+						System.out.println("Roll Number: ");
+						rollno = scnr.nextInt();
+					}catch (InputMismatchException e) {
+						System.out.println("Invalid entry. Please enter again.");
+						scnr.next();
+					}
 				}
-			}
 		
-			scnr.nextLine();
-			System.out.println("Address: ");
-			String address = scnr.nextLine();
-			students.add(new Student(name, rollno, address));
-			i++;
-			
+				scnr.nextLine();
+					System.out.println("Address: ");
+				String address = scnr.nextLine();
+				students.add(new Student(name, rollno, address));
+				i++;
+			}
+			scnr.close();
+		}catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
 		}
-		scnr.close();
+		
 		System.out.println("Students Added.\n");
 	
 		System.out.println("Sorting by Name: ");
